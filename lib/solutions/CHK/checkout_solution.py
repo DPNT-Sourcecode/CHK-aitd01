e987340351c0c4bd2ca64bc68e9c25b7abae668e
sbx_user1051 <sbx_user1051@169.254.216.141>
2022-06-07 17:57:01 +0000

    Tue Jun 07 17:57:01 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict
 
 PRICES = {
     "A": 50,
@@ -21,4 +21,20 @@ OFFERS = {
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    skus.split()
+    shopping = defaultdict(int)
+    for item in skus.split():
+        shopping[item] += 1
+        
+
+    total = 0
+    for item, count in shopping.items():
+        offer = OFFERS.get(item) 
+        if not offer:
+            total += count * PRICES[item]
+        else:
+            multibuys_count = count // offer["count"]
+            remainder = count % offer["count"]
+            total += multibuys_count * offer["price"] + remainder * PRICES[item]
+
+    return total
+
