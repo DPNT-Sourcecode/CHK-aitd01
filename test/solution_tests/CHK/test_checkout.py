b6d11a41741f9ffde595370f99a0e356ea2665c3
sbx_user1051 <sbx_user1051@169.254.216.141>
2022-06-07 18:03:01 +0000

    Tue Jun 07 18:03:01 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,8 +1,8 @@
 from solutions.CHK import checkout_solution
 
 
-class TestSum():
-    def test_sum(self):
+class TestCheckout():
+    def test_checkout(self):
         assert checkout_solution.checkout("A") == 50
         assert checkout_solution.checkout("B") == 30
         assert checkout_solution.checkout("C") == 20
@@ -12,4 +12,12 @@ class TestSum():
         assert checkout_solution.checkout("A A A") == 130
         assert checkout_solution.checkout("A A A A A") == 230
         assert checkout_solution.checkout("A A A A A A A") == 310
+
+
+        assert checkout_solution.checkout("B B") == 45
+        assert checkout_solution.checkout("B B B") == 75
+        assert checkout_solution.checkout("B B B B") == 90
+        assert checkout_solution.checkout("B B B B B") == 120
+
+        assert checkout_solution.checkout("B B B B") == 90
 
