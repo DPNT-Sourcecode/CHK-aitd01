2a3b033549dd31f9d0e233097b2129a9ddcc5205
sbx_user1051 <sbx_user1051@169.254.216.141>
2022-06-07 18:41:37 +0000

    Tue Jun 07 18:41:37 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -36,30 +36,58 @@ OFFERS = {
     }
 }
 
+
+class Shopping():
+
+    def __init__(self):
+        self.items = defaultdict(lambda : {"count": 0, "subtotal": 0})
+
+    def add(self, item):
+        self.items[item]["count"] += 1
+        self.items[item]["subtotal"] += PRICES[item]
+
+        
+
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     if skus == None:
         return -1
 
-    shopping = defaultdict(int)
+    shopping = defaultdict(lambda : {"count": 0, "subtotal": 0})
     for item in skus:
         if item not in PRICES:
             return -1
 
-        shopping[item] += 1
+        shopping[item]["count"] += 1
+        shopping[item]["subtotal"] += PRICES[item]
+
 
     total = 0
+    # apply offers
+    for item, deals in OFFERS:
+        if item not in shopping:
+            continue
+
+        # apply freebes
+        if "free" in deals:
+
+
+
+
     for item, count in shopping.items():
         offer = OFFERS.get(item)
         if not offer:
             total += count * PRICES[item]
         else:
-            multibuys_count = count // offer["count"]
-            remainder = count % offer["count"]
-            total += multibuys_count * offer["price"] + remainder * PRICES[item]
+            if "multibuy" in offer:
+                total += calc_multibuy(item, count)
+
+            # multibuys_count = count // offer["multibuy"]["count"]
+            # remainder = count % offer["count"]
+            # total += multibuys_count * offer["price"] + remainder * PRICES[item]
+
+
 
     return total
-
-
-
