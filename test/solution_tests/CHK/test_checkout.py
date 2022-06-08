6446deea22bbf07a515cb0c443c420f2961d9304
sbx_user1051 <sbx_user1051@169.254.0.245>
2022-06-08 14:32:28 +0000

    Wed Jun 08 14:32:28 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,28 +3,28 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_checkout(self):
-        # assert checkout_solution.checkout("A") == 50
-        # assert checkout_solution.checkout("B") == 30
-        # assert checkout_solution.checkout("C") == 20
-        # assert checkout_solution.checkout("D") == 15
-        # assert checkout_solution.checkout("E") == 40
-        # assert checkout_solution.checkout("F") == 10
+        assert checkout_solution.checkout("A") == 50
+        assert checkout_solution.checkout("B") == 30
+        assert checkout_solution.checkout("C") == 20
+        assert checkout_solution.checkout("D") == 15
+        assert checkout_solution.checkout("E") == 40
+        assert checkout_solution.checkout("F") == 10
 
-        # assert checkout_solution.checkout("AA") == 100
-        # assert checkout_solution.checkout("AAA") == 130
-        # assert checkout_solution.checkout("AAAAA") == 200
-        # assert checkout_solution.checkout("AAAAAAA") == 300
-        # assert checkout_solution.checkout("AAAAAAAA") == 330
-        # assert checkout_solution.checkout("AAAAAAAAA") == 380
-        # assert checkout_solution.checkout("AAAAAAAAAA") == 400
-        # assert checkout_solution.checkout("AAAAAAAAAAAA") == 500 # 12A
-        # assert checkout_solution.checkout("AAAAAAAAAAAAA") == 530 # 13A
+        assert checkout_solution.checkout("AA") == 100
+        assert checkout_solution.checkout("AAA") == 130
+        assert checkout_solution.checkout("AAAAA") == 200
+        assert checkout_solution.checkout("AAAAAAA") == 300
+        assert checkout_solution.checkout("AAAAAAAA") == 330
+        assert checkout_solution.checkout("AAAAAAAAA") == 380
+        assert checkout_solution.checkout("AAAAAAAAAA") == 400
+        assert checkout_solution.checkout("AAAAAAAAAAAA") == 500 # 12A
+        assert checkout_solution.checkout("AAAAAAAAAAAAA") == 530 # 13A
 
 
-        # assert checkout_solution.checkout("BB") == 45
-        # assert checkout_solution.checkout("BBB") == 75
-        # assert checkout_solution.checkout("BBBB") == 90
-        # assert checkout_solution.checkout("BBBBB") == 120
+        assert checkout_solution.checkout("BB") == 45
+        assert checkout_solution.checkout("BBB") == 75
+        assert checkout_solution.checkout("BBBB") == 90
+        assert checkout_solution.checkout("BBBBB") == 120
 
         assert checkout_solution.checkout("EE") == 80
         assert checkout_solution.checkout("EEB") == 80
@@ -52,4 +52,5 @@ class TestCheckout():
         assert checkout_solution.checkout(None) == -1
         assert checkout_solution.checkout("a") == -1
         assert checkout_solution.checkout("ADBCBBaACxCDAA") == -1
+
 
